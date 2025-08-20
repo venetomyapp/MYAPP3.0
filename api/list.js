861dfc07b1b5ffb3822c798cf43948daa74244e1
venetomyapp <veneto.nsc@gmail.com>
2025-08-20 20:16:58 +0200

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,7 +1,6 @@
-// pages/api/list.js
 import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
-const s3 = new S3Client({
+const client = new S3Client({
   region: "auto",
   endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
@@ -12,16 +11,11 @@ const s3 = new S3Client({
 
 export default async function handler(req, res) {
   try {
-    const command = new ListObjectsV2Command({
-      Bucket: process.env.R2_BUCKET,
-    });
-
-    const response = await s3.send(command);
-    const files = (response.Contents || []).map((f) => f.Key);
-
-    return res.status(200).json({ ok: true, files });
+    const command = new ListObjectsV2Command({ Bucket: process.env.R2_BUCKET });
+    const data = await client.send(command);
+    res.status(200).json(data.Contents || []);
   } catch (err) {
-    console.error("Errore list:", err);
-    return res.status(500).json({ error: "Errore nel listare i file", details: err.message });
+    console.error("List error:", err);
+    res.status(500).json({ error: "Errore lista file", details: err.message });
   }
 }
