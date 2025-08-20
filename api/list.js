d129f79b19fdc607ebddba6329f4730998661e34
venetomyapp <veneto.nsc@gmail.com>
2025-08-20 21:32:37 +0200

    Update list.js

--- api/list.js
+++ api/list.js
@@ -2,12 +2,11 @@ import { S3Client, ListObjectsV2Command } from "@aws-sdk/client-s3";
 
 const client = new S3Client({
   region: "auto",
-  endpoint: `https://${process.env.R2_BUCKET}.${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+  endpoint: `https://${process.env.R2_ACCOUNT_ID}.r2.cloudflarestorage.com`,
   credentials: {
     accessKeyId: process.env.R2_ACCESS_KEY_ID,
     secretAccessKey: process.env.R2_SECRET_ACCESS_KEY,
-  },
-  forcePathStyle: true,
+  }
 });
 
 export default async function handler(req, res) {
